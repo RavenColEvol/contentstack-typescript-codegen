6a7aeb062c93defef9295d6f174f43d42f46712d
RavenColEvol <ravilamkoti@gmail.com>
2023-10-29 20:28:11 +0530

    initial commit

---
+++ test/generator.spec.ts
@@ -0,0 +1,6 @@
+import { TypesGenerator } from '../src/utils';
+
+describe('TypesGenerator', () => {
+  it.todo('should be able to add content types');
+  it.todo('should be able to types as string');
+})
